f6294ab54bf2ae98541e5771c2f740de493671a8
RyanMitchell8 <N00233039@iadt.ie>
2024-10-30 15:41:09 +0000

    colour change on view call cars

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('All Cars') }}
+        <h2 class="font-semibold text-xl text-black bg-blue-500 leading-tight">
+        {{ __('All Cars') }}
         </h2>
     </x-slot>
  
-    <div class="py-12">
+    <div class="py-12 bg-gray-200">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div class="p-6 text-gray-900 bg-gray-200">
                     <h3 class="font-semibold text-lg mb-4">List of Cars:</h3>
                     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                         @foreach($cars as $car)
@@ -18,6 +18,7 @@
                                 :type="$car->type"
                                 :image_url="$car->image_url"
                                 :year="$car->year"
+                                class="bg-blue-500 hover:bg-blue-600 text-black"
                             />
                         </a>
                         @endforeach
